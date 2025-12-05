34f84b826136f460314d4a0825f55e33501c99fd
Jonas Tholl <jonas@tholls.de>
2025-12-05 10:28:13 +0100

    added Method deklaration

--- src/main/java/PasswordValidation.java
+++ src/main/java/PasswordValidation.java
@@ -1,5 +1,30 @@
-public class PasswordValidation {
-    static void main() {
-        System.out.println("Hello World");
+public final class PasswordValidation {
+
+    static boolean hasMinLenght(String password, int min) {
+        //TODO
+    }
+
+    static boolean containsDigit (String password) {
+        //TODO
+    }
+
+    static boolean containsUpperChar (String password) {
+        //TODO
+    }
+
+    static boolean containsLowerChar (String password) {
+        //TODO
+    }
+
+    static boolean isCommonPassword (String password) {
+        //TODO
+    }
+
+    static boolean containsSpecialChar (String password, String allowed) {
+        //TODO
+    }
+
+    static boolean isValid (String password) {
+        //TODO
     }
 }
